11434ec0731d66a03f21b515bb0588ce97980f0e
Roman <romzayac@gmail.com>
2016-10-21 17:44:54 +0300

    Update DishParser.cs

--- RecipeBookSystem.DAL/Concrete/Parsers/DishParser.cs
+++ RecipeBookSystem.DAL/Concrete/Parsers/DishParser.cs
@@ -8,6 +8,8 @@ namespace RecipeBookSystem.DAL.Concrete.Parsers
     {
         public static DishModel MakeBuildingResult(SqlDataReader reader)
         {
+            //Review RZ: it would be greate to use object initializer
+            
             var model = new DishModel();
 
             model.Id = reader.Get<int>(ColumnNames.DishId);
